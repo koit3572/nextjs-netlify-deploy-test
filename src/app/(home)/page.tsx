1689556f7888afe62a473b461e951c97fcc12d41
koit <koit3572@gmail.com>
2024-08-03 02:00:54 +0900

    2024-08-02

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,18 +19,18 @@ import { useEffect } from 'react';
 // }
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-  const { photos,isLoading } = useAppSelector(state => state.photosSlice)
-  useEffect(() => {
-    if (Object.keys(photos).length === 0) {
-      dispatch(fetchPhotos())
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  useEffect(() => {
-    console.log("photos", photos);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[isLoading])
+  // const dispatch = useAppDispatch();
+  // const { photos,isLoading } = useAppSelector(state => state.photosSlice)
+  // useEffect(() => {
+  //   if (Object.keys(photos).length === 0) {
+  //     dispatch(fetchPhotos())
+  //   }
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
+  // useEffect(() => {
+  //   console.log("photos", photos);
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // },[isLoading])
   return (
    <div>안녕하세요</div>
   )
