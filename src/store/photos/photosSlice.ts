02dc2e232fe417f0656a756969d6793494ff7ca8
koit <koit3572@gmail.com>
2024-08-02 23:39:52 +0900

    2024-08-02

--- src/store/photos/photosSlice.ts
+++ src/store/photos/photosSlice.ts
@@ -1,68 +1,71 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-// interface IPhotos {
-//   albumId: number;
-//   id: number;
-//   title: string;
-//   url: string;
-//   thumbnailUrl: string;
-// }
-interface PostData {
+interface IPhotos {
+  albumId: number;
+  id: number;
   title: string;
-  discription: string;
-  writer: string;
-  createdAt: string;
-  updatedAt: string;
-  tags: string[];
-  content?: string;
-  isFavorite: boolean;
+  url: string;
+  thumbnailUrl: string;
 }
-interface IMainSideBarData {
-  [dirName: string]: string[];
-}
-interface IPostData {
-  postFolderStructure: IMainSideBarData;
-  posts: {
-    [postPath: string]: PostData;
-  };
-}
+
+// interface PostData {
+//   title: string;
+//   discription: string;
+//   writer: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   tags: string[];
+//   content?: string;
+//   isFavorite: boolean;
+// }
+// interface IMainSideBarData {
+//   [dirName: string]: string[];
+// }
+// interface IPostData {
+//   postFolderStructure: IMainSideBarData;
+//   posts: {
+//     [postPath: string]: PostData;
+//   };
+// }
+
 interface IInitialState {
-  photos: IPostData;
+  photos: IPhotos[];
   isLoading: boolean;
   error: string;
 }
 const initialState: IInitialState = {
-  photos: {} as IPostData,
+  photos: [], //{} as IPostData,
   isLoading: true,
   error: "",
 };
 export const fetchPhotos = createAsyncThunk(
   "photos/fetchPhotos",
   async (_, thunkAPI) => {
-    try {
-      const res = await fetch("/api", {
-        method: "GET",
-        cache: "no-store",
-      });
-      if (!res) {
-        throw new Error("error response is empty");
-      }
-      const postData = await res.json();
-      return postData;
-    } catch (error) {
-      return thunkAPI.rejectWithValue("Error loading fetchPost");
-    }
     // try {
-    //   const url = "api";
-    //   const res = await fetch(url, {
+    //   const res = await fetch("/api", {
     //     method: "GET",
     //     cache: "no-store",
     //   });
-    //   const photos = await res.json();
-    //   return photos;
+    //   if (!res) {
+    //     throw new Error("error response is empty");
+    //   }
+    //   const postData = await res.json();
+    //   return postData;
     // } catch (error) {
-    //   console.error(error);
+    //   return thunkAPI.rejectWithValue("Error loading fetchPost");
     // }
+
+    try {
+      const url = "api";
+      const res = await fetch(url, {
+        method: "GET",
+        cache: "no-store",
+      });
+      const photos = await res.json();
+      return photos;
+    } catch (error) {
+      console.error(error);
+    }
   }
 )
 
